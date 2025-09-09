d8b5602f10a7eb1b4c5d019832e6eee8537fb2de
Adzipurnomo18 <adzypurnomo@gmail.com>
2025-09-09 16:01:01 +0700

    edit 10

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.querySelector(".download-btn").addEventListener("click", function() {
     window.print(); // Trigger the print dialog
 });
 
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
